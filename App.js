5a2970f99fe3a23a460792e2064d4befb72c92d2
gabrielaelaela <ticugabriela12@gmail.com>
2023-08-24 23:40:15 +0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import {useState} from 'react';
 
 export default function App() {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+  
+  const add = () => {
+    setItems([...items, {key: item}]);
+    setItem("");
+  }
+  
+  const clear = () => {
+    setItems([]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={{width: 200, borderColor: 'gray', borderWidth: 1}}
+                  onChangeText={item => setItem(item)} value={item}/>
+      <View style={styles.view}>
+        <Button title="Add" onPress={add} />
+        <Button title="Clear" onPress={clear} />
+      </View>
+      <Text style={{fontWeight: "bold"}}>Shopping list</Text>
+      <FlatList style={{flex: 0.4}} data={items} renderItem={({item}) => <Text>{item.key}</Text>}
+                keyExtractor={(item, index) => index.toString()} scrollEnabled={true}/>
     </View>
   );
 }
@@ -16,5 +35,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 150
   },
+  view: {
+    flex: 0.4,
+    flexDirection: "row",
+    alignItems: "flex-start",
+    marginTop: 20
+  }
 });
